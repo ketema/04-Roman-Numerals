53fd23fc12e4364222c182014886d49361571d0b
Ketema Harris <ketema@ketema.net>
2010-07-03 15:23:03 -0400

    Changed up this object to suit me a little more.  used a fixture in the unit tests.  attempted to solve the decimal to roman portion.  only partial success. comments in code

--- RomanNumeral.php
+++ RomanNumeral.php
@@ -2,46 +2,102 @@
 
 class RomanNumeral
 {
-    public function __construct ( $numeral )
-    {
-        $this->_value = $numeral;
-        $this->_lookup = array (
-        ''  => 0,    
-        'I' => 1,
-        'V' => 5,
-        'X' => 10,
-        'L' => 50,
-        'C' => 100,
-        'D' => 500,
-        'M' => 1000,
+    private $_decimalValue = null;
+    private $_romanValue = null;
+    private $_lookup = array (
+        ''   => 0,    
+        'I'  => 1,
+        'V'  => 5,
+        'X'  => 10,
+        'L'  => 50,
+        'C'  => 100,
+        'D'  => 500,
+        'M'  => 1000,
         );
-
-    }
-
-    public function toInt ( )
+    
+    public function __construct ( $romanNumeral = null )
     {
-        return $this->addEmUp($this->parse());
+        $this->_romanValue   = $romanNumeral;
+        $this->_decimalValue = $this->toInt( $romanNumeral );
+
     }
 
-    public function parse() {
-        return str_split($this->_value );
+    public function parseRoman( $romanNumeral = '' ) {
+        if( empty( $romanNumeral ) ){
+            return str_split($this->_romanValue );
+        }
+        else{
+            return str_split( $romanNumeral );
+        }
     }
 
-    public function addEmUp($numerals) {
-        $calculated = 0;
-        $last       = '';
-        $numerals   = array_reverse($numerals);
+    public function toInt( $romanNumeral ) {
+        $calculated    = 0;
+        $last          = '';
+        $romanNumerals = array_reverse( $this->parseRoman( $romanNumeral ) );
         
-        foreach ($numerals as $numeral) {
-            if( $this->_lookup[$numeral] < $this->_lookup[$last] ){
-                $calculated -= $this->_lookup[$numeral];
+        foreach ($romanNumerals as $romanNumeral) {
+            if( $this->_lookup[$romanNumeral] < $this->_lookup[$last] ){
+                $calculated -= $this->_lookup[$romanNumeral];
             }
             else{
-                $calculated += $this->_lookup[$numeral];
+                $calculated += $this->_lookup[$romanNumeral];
             }
-            $last = $numeral;
+            $last = $romanNumeral;
         }
         return $calculated;
+    }
+
+    public function fromInt( $integer ){
+        /**
+         * we use our existing lookup array and just flip it locally to be the 
+         * lookup array for decimal to roman numeral
+         */
+        $lookup       = array_flip( $this->_lookup );
+        $romanNumeral = '';
+
+        //Make sure we have an integer
+        $integer = intval( $integer );
+
+        if ( $integer == 0 ) return ''; //Is there a roman 0 ?
+        
+        /**
+         * The decimal has to be converted to a string then
+         * broken down into its positional values
+         * once the positional value integers are obtained they
+         * have to be multiplied by ten to get their value
+         * once we have the value we look it up to get the
+         * roman representation the concatenate the string
+         * this is totally BS, but is a good place to start
+         * I have got it to where for base ten numbers it works but the problem 
+         * is that the stupid romans did not use base ten, then have 500 and 5 
+         * and 50 and they don't have 0 as far as I can tell I just added it 
+         * in. In order to pass the test for the failing data I have to figure 
+         * out the logic for these altered bases as well as the subtraction 
+         * logic.
+         **/
+
+        //Take the integer and turn it into a string then reverse it cause we 
+        //use a place holding nubering system with lower vals on the right
+        $integer = array_reverse( str_split( $integer ) );
+
+        /**
+         * Loop over our integer string array using the index as the positional 
+         * place holder, and the value tells us how many time we need that 
+         * roman numeral.  This currently does not take into account the 
+         * subtraction conditions that the roman system uses
+         */
+        //@TODO Figure out when to do  subtraction stuff and how to get those 
+        //bases that our ten base system does not use, i.e. 500 and 50
+        foreach( $integer as $position => $value ){
+            $lookupIndex = pow(10, ( $position ) );
+            for( $i = 0; $i < $value; $i++ ){
+                $romanNumeral .= $lookup[$lookupIndex];
+            }
+        }
+        //We return our roman numeral string in reverse order cause the romans 
+        //are even more backward than we are.
+        return implode( array_reverse( str_split( $romanNumeral ) ) );
     }
 
 }
--- RomanNumeralTest.php
+++ RomanNumeralTest.php
@@ -6,6 +6,7 @@ require_once 'RomanNumeral.php';
 class MainTest
 extends PHPUnit_Framework_TestCase
 {
+
     public function provide_numerals ( )
     {
         return array(
@@ -32,18 +33,30 @@ extends PHPUnit_Framework_TestCase
         ); // END datasets
     }
 
-    /**
-     * @dataProvider provide_numerals
-     * @param integer $expected
-     * @param string $numeral
-     */
-    public function test_basic_stuff ( $expected, $numeral )
-    {
-        $Numeral = new RomanNumeral($numeral);
+    public function provide_decimals(){
+        return array(
+            array( '', 0 ),
+            array( 'I', 1 ),
+            array( 'II', 2 ),
+            array( 'III', 3 ),
+            array( 'IV', 4 ),
+            array( 'V', 5 ),
+            array( 'IX', 9 ),
+            array( 'X', 10 ),
+            array( 'XI', 11 ),
+            array( 'XII', 12 ),
+            array( 'XIV', 14 ),
+            array( 'XIX', 19 ),
+            array( 'XXX', 30 ),
+            array( 'XL', 40 ),
+            array( 'L', 50 ),
+            array( 'C', 100 ),
+            array( 'D', 500 ),
+            array( 'M', 1000 ),
+            array( 'MCCXXXIV', 1234 ),
+        );
+    } //END provide_decimals
 
-        $this->assertEquals($expected, $Numeral->toInt());
-
-    }
     public function provide_parsables(){
         return array(
             array(array('I'), 'I'),
@@ -54,27 +67,39 @@ extends PHPUnit_Framework_TestCase
         );
     }
 
+    public function setup(){
+        $this->fixture = new RomanNumeral;
+        $this->assertTrue( ($this->fixture instanceof RomanNumeral), 'This is not a roman numeral' );
+    } //END setup
+
     /**
      * @dataProvider provide_parsables
      * @param integer $expected
      * @param string $numeral
      */
-    public function test_parse( $expected, $numeral ){
-        $Numeral = new RomanNumeral($numeral);
-        $this->assertEquals($expected, $Numeral->parse());
+    public function test_parse( $expected, $RomanNumeral ){
+        $this->assertEquals($expected, $this->fixture->parseRoman( $RomanNumeral ));
     }
 
 
      /**
      * @dataProvider provide_numerals
      * @param integer $expected
-     * @param string $numeral
+     * @param string $romanNumeral
      */
-    public function test_addEmUp($expected, $numeral) {
-        $Numeral = new RomanNumeral($numeral);
-        $parsed = $Numeral->parse();
-        $this->assertEquals($expected, $Numeral->addEmUp($parsed));
+    public function test_toInt( $expected, $romanNumeral ) {
+        $this->assertEquals($expected, $this->fixture->toInt( $romanNumeral ) );
     }
+
+
+     /**
+     * @dataProvider provide_decimals
+     * @param string $expected
+     * @param integer $integer
+     */
+    public function test_fromInt( $expected, $integer ){
+        $this->assertEquals( $expected, $this->fixture->fromInt( $integer ) );
+    } //END test_fromDecimal 
 
 } // END RomanNumeralTest
 
